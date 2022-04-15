6cc5211c6ec7e12093af28546d338cfb5c5056eb
sbx_user1051 <sbx_user1051@169.254.0.105>
2022-04-15 08:06:15 +0000

    Fri Apr 15 08:06:15 UTC 2022

--- test/solution_tests/HLO/test_hlo.py
+++ test/solution_tests/HLO/test_hlo.py
@@ -1,10 +1,13 @@
 import unittest
+import solutions.HLO.hello_solution as hello_solution
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, True)  # add assertion here
+class HelloTestCase(unittest.TestCase):
+    def test_hello_george(self):
+        res = hello_solution.hello("George")
+        self.assertEqual(res, "Hello George")  # add assertion here
 
 
 if __name__ == '__main__':
     unittest.main()
+
