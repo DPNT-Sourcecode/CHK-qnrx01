aea185838517514b0566eb479ac9f1806d9f0aed
sbx_user1051 <sbx_user1051@169.254.0.105>
2022-04-15 08:13:38 +0000

    Fri Apr 15 08:13:38 UTC 2022

--- test/solution_tests/HLO/test_hlo.py
+++ test/solution_tests/HLO/test_hlo.py
@@ -2,8 +2,9 @@ import solutions.HLO.hello_solution as hello_solution
 
 
 class TestSum:
-    def test_hello_george(self):
-        res = hello_solution.hello("George")
-        assert res == "Hello George"
+    def test_hello_john(self):
+        res = hello_solution.hello("John")
+        assert res == "Hello, John!"
+
 
 
